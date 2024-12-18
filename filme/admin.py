ba738296952a49a854e1bb13310225cd9b01e86f
Rodrigo Costa <rodrigo.costa94@yahoo.com.br>
2024-12-18 11:11:38 -0300

    criando o primeiro filme

--- filme/admin.py
+++ filme/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Filme          # O .models "PONTO MODELS",  o PONTO vem de pegar o arquivo models que está na mesma pasta (ESSA LINHA FOI ADICIONADA MANUALMENTE)
+
 # Register your models here.
+admin.site.register(Filme)
--- filme/models.py
+++ filme/models.py
@@ -13,8 +13,8 @@ LISTA_CATEGORIAS = (
 
 # Criar o filme
 
-# A classe filme é uma subclasse, por isso por padrão ela receebe no parenteses (models.Model)
-class Filme(models.Model):
+# A classe filme é uma subclasse, por isso por padrão ela recebe no parenteses (models.Model)
+class Filme(models.Model): # NA TELA DE ADMIN O NOME DA CLASSE FILME VAI FICAR NO PLURAL ( S )
     titulo = models.CharField(max_length=100)  # CHARFIELD É UM PEQUENO CAMPO DE TEXTO, foi definido o maxlength para limitar o numero de caracteres
     thumb = models.ImageField(upload_to='thumb_filmes')
     descricao = models.TextField(max_length=1000) # TEXTFIELD É UM CAMPO DE TEXTO MAIOR, foi definido o maxlength para limitar o numero de caracteres
@@ -22,5 +22,9 @@ class Filme(models.Model):
     visualizacoes = models.IntegerField(default=0) # INTEGERFIELD É UM VALOR NUMÉRICO
     data_criacao = models.DateTimeField(default=timezone.now)
     
+    # Essa função diz para cada classe do python o que vai aparecer quando o usuário solicitar um print em algum item dessa classe
+    def __str__(self):
+        # Essa função muda o nome de Filme object (1) para o nome que voce definir
+        return self.titulo  #retorna o titulo do filme
 
 # Criar os episodios
